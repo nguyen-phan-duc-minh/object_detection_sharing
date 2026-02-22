2e45aed983e59192b7dd8a4c4cb60dd9f5d97ed9
Your Name <macos@MacBook-Air-cua-MACOS.local>
2026-02-22 14:07:37 +0700

    object detecion co ban va counting voi line + zone co ban chua tracking

--- object-detection-basic/main.py
+++ object-detection-basic/main.py
@@ -1,19 +1,20 @@
 import cv2
 from detector import YOLODetector
-# from counter import LineCounter
-# from counter import ZoneCounter
+from counter import LineCounter
+from counter import ZoneCounter
+from counter import LaneZoneCounter
 from visualize import draw_boxes
 from saver import ResultSaver
 
 VIDEO_PATH = "inputs/videos/videoplayback.mp4"
-VID_STRIDE = 3
+VID_STRIDE = 1
 
 def main():
     detector = YOLODetector(
         "models/yolo26n.pt",
         # conf=0.4,
         # iou=0.6,
-        # classes=[0],  # chỉ detect person
+        classes=[2],  # chỉ detect person
         # max_det=50,
     )
     saver = ResultSaver(
@@ -22,29 +23,35 @@ def main():
         save_txt=False,
         save_crop=False,
     )
-    # counter = LineCounter((100, 300), (500, 300))
-    # counter = ZoneCounter((200, 200), (500, 500))
     cap = cv2.VideoCapture(VIDEO_PATH)
+    ret, frame = cap.read()
+    if not ret:
+        return
+    
+    counter = LineCounter((100, 200), (500, 200))
+    # counter = ZoneCounter((100, 100), (500, 300))
+    # points = [(180, 100), (400, 100), (550, 300), (50, 300)]
+    # counter = LaneZoneCounter(points, frame.shape)
     frame_id = 0
     detections = []
 
     while True:
-        ret, frame = cap.read()
-        if not ret:
-            break
-        
         frame_id += 1
         
         if frame_id == 1 or frame_id % VID_STRIDE == 0:
             detections = detector.detect(frame)
             saver.save(frame, detections, frame_id)
 
-        # counter.update(detections)
+        counter.update(detections)
         frame = draw_boxes(frame, detections, detector.model.names)
-        # frame = counter.draw(frame)
+        frame = counter.draw(frame)
         cv2.imshow("Detection", frame)
 
         if cv2.waitKey(1) == 27:
+            break
+        
+        ret, frame = cap.read()
+        if not ret:
             break
 
     cap.release()
